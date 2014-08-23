1893cad333ffe1e88e4d8e4ef3d9cc1686d298a9
ehemdal <ehemdal@townisp.com>
2014-08-23 14:41:55 -0400

    Adding best.R; fixed state validation per TA advice

--- best.R
+++ best.R
@@ -1,15 +1,21 @@
 best <- function(state, outcome) {
   ## Read outcome data
   setwd("C:/git/ProgrammingAssignment3")
-  data <-read.csv("outcome-of-care-measures.csv") #start w/out colClasses="character"
- 
+  d <-read.csv("outcome-of-care-measures.csv",colClasses="character") #start w/out colClasses="character"
+  
   ## Check that state and outcome are valid
-  if (sum(state == state.abb) < 1) { stop("invalid state")}
-  else { print("OK state")}
-  ocname <- c("heart attack", "heart failure","pneumonia")
-  if (sum(outcome == ocname) < 1) {stop("invalid outcome")}
-  else {print("OK outcome")}
+  if (sum(state == unique(d[,7])) != 1) { stop("invalid state")}
+  #else { print("OK state")}
+  ocname <- c("heart attack" = 11, "heart failure" =17 ,"pneumonia" = 23)
+  
+  if (sum(outcome == names(ocname)) != 1) {stop("invalid outcome")}
+  #else {print("OK outcome")}
 
   ## Return hospital name in that state with lowest 30-day death
   ## rate
+  c <- as.numeric(ocname[outcome])
+  s <- d[,c]
+  s <-as.numeric(s)
+  k <- s==min(s)
+  return()
 }
