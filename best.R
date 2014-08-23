d8dbf8d397e1e92a0d2c9b55ccf5ebfe13635e81
ehemdal <ehemdal@townisp.com>
2014-08-23 12:49:44 -0400

    Adding best.R; fixed data read; outcome validation works

--- best.R
+++ best.R
@@ -1,11 +1,15 @@
 best <- function(state, outcome) {
   ## Read outcome data
   setwd("C:/git/ProgrammingAssignment3")
-  outcome <-read.csv("outcome-of-care-measures.csv") #start w/out colClasses="character"
+  data <-read.csv("outcome-of-care-measures.csv") #start w/out colClasses="character"
  
   ## Check that state and outcome are valid
   if (sum(state == state.abb) < 1) { stop("invalid state")}
-  else { print("OK")}
+  else { print("OK state")}
+  ocname <- c("heart attack", "heart failure","pneumonia")
+  if (sum(outcome == ocname) < 1) {stop("invalid outcome")}
+  else {print("OK outcome")}
+
   ## Return hospital name in that state with lowest 30-day death
   ## rate
 }
